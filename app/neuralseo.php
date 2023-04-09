e669c63ec59b1025498d23a30ce601230ee0f9a2
Igor Tron <public@itron.pro>
2023-04-09 17:52:03 +0200

    plugin got created

---
+++ app/neuralseo.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* Plugin Name: NeuralSEO
+* Description: Integrate your site with Neural SEO API
+* Author:      Hokku
+* Version:     0.1
+* License:     GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: neuralseo
+* Domain Path: /languages
+*/
