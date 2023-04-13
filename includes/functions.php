e7b5bded6d7c0b915864129ae7dc0348b25298ec
Igor Tron <public@itron.pro>
2023-04-13 18:46:33 +0200

    get_action_status_by_id

--- includes/functions.php
+++ includes/functions.php
@@ -2,6 +2,7 @@
 
 namespace NeuralSEO;
 
+use ActionScheduler;
 use NeuralApi\DevClient;
 
 function get_client() {
@@ -15,4 +16,19 @@ function get_client() {
 	return $client;
 }
 
+function get_action_status_by_id( $action_id ) {
+	// Make sure Action Scheduler is loaded
+	if (!class_exists('ActionScheduler')) {
+		return false;
+	}
 
+	// Get the action from the ActionScheduler store
+	$action = ActionScheduler::store()->fetch_action($action_id);
+
+	// Check if the action exists
+	if ($action) {
+		return ActionScheduler::store()->get_status($action_id);
+	} else {
+		return false;
+	}
+}
