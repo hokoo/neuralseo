2f80e4232f48432cea20e7b20a91e5676943fde9
Igor Tron <public@itron.pro>
2023-05-18 17:16:20 +0200

    tests' container got added

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,12 @@
 <?php
+$test_root = getenv( 'WP_TESTS_DIR' ) ? : dirname( __FILE__ ) . '/../wordpress-develop/tests/phpunit';
+define( "WP_TESTS_CONFIG_FILE_PATH", $test_root . '/../../wp-tests-config.php' );
+require $test_root . '/includes/functions.php';
 
-declare(strict_types=1);
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../neuralseo.php';
+}
+echo $test_root;
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-const UNIT_TESTS = true;
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require $test_root . '/includes/bootstrap.php';
